bb6315bdef4934a829194300346ade2da75e2eee
Paul Franco <mail@paulfran.co>
2021-01-04 01:18:02 -0800

    set up the auth fragment

--- app/src/main/java/co/paulfran/ktor_note_app_client/ui/auth/AuthFragment.kt
+++ app/src/main/java/co/paulfran/ktor_note_app_client/ui/auth/AuthFragment.kt
@@ -1,15 +1,55 @@
 package co.paulfran.ktor_note_app_client.ui.auth
 
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import co.paulfran.ktor_note_app_client.R
+import co.paulfran.ktor_note_app_client.other.Status
 import co.paulfran.ktor_note_app_client.ui.BaseFragment
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_auth.*
 
+@AndroidEntryPoint
 class AuthFragment : BaseFragment(R.layout.fragment_auth){
+    // get reference to the view model
+    private val viewModel: AuthViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        // disable landscape mode
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
+        subscribeTpObservers()
+        btnRegister.setOnClickListener {
+            val email = etRegisterEmail.text.toString()
+            val password = etRegisterPassword.text.toString()
+            val confirmPassword = etRegisterPasswordConfirm.text.toString()
+            viewModel.register(email, password, confirmPassword)
+        }
+
+    }
+
+    private fun subscribeTpObservers() {
+        viewModel.registerStatus.observe(viewLifecycleOwner, Observer { result ->
+            result?.let {
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        registerProgressBar.visibility = View.GONE
+                        showSnackBar(result.data ?: "Successfully registered an account")
+                    }
+                    Status.ERROR -> {
+                        registerProgressBar.visibility = View.GONE
+                        showSnackBar(result.message ?: "Unknown error occurred")
+                    }
+                    Status.LOADING -> {
+                        registerProgressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
     }
 }
