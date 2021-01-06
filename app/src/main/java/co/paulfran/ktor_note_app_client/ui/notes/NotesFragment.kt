de82158ca7e6e24ae1d8ffb69130415341cff1aa
Paul Franco <mail@paulfran.co>
2021-01-06 03:57:31 -0800

    show notes in the NotesFragment

--- app/src/main/java/co/paulfran/ktor_note_app_client/ui/notes/NotesFragment.kt
+++ app/src/main/java/co/paulfran/ktor_note_app_client/ui/notes/NotesFragment.kt
@@ -1,18 +1,122 @@
 package co.paulfran.ktor_note_app_client.ui.notes
 
+import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
-import android.view.View
+import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.paulfran.ktor_note_app_client.R
+import co.paulfran.ktor_note_app_client.adapters.NoteAdapter
+import co.paulfran.ktor_note_app_client.other.Constants.KEY_LOGGED_IN_EMAIL
+import co.paulfran.ktor_note_app_client.other.Constants.KEY_LOGGED_IN_PASSWORD
+import co.paulfran.ktor_note_app_client.other.Constants.NO_EMAIL
+import co.paulfran.ktor_note_app_client.other.Constants.NO_PASSWORD
+import co.paulfran.ktor_note_app_client.other.Status
 import co.paulfran.ktor_note_app_client.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NotesFragment : BaseFragment(R.layout.fragment_notes) {
+
+    private val viewModel: NotesViewModel by viewModels()
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    private lateinit var noteAdapter: NoteAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+        setupRecyclerView()
+        subscribeToObservers()
+
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                    NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
+
         fabAddNote.setOnClickListener {
             findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+            it?.let { event ->
+                val result = event.peekContent()
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!!
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = true
+                    }
+                    Status.ERROR -> {
+                        event.getContentIfNotHandled()?.let { errorResource ->
+                            errorResource.message?.let { message ->
+                                showSnackBar(message)
+                            }
+                        }
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() = rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+
+
+    private fun logout() {
+        sharedPref.edit().putString(KEY_LOGGED_IN_EMAIL, NO_EMAIL).apply()
+        sharedPref.edit().putString(KEY_LOGGED_IN_PASSWORD, NO_PASSWORD).apply()
+
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.notesFragment, true)
+            .build()
+
+        findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAuthFragment(), navOptions)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_notes, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miLogout -> logout()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
