a09ec9bfbd08cf0b68fda33d729ee50d7c4869d7
Paul Franco <mail@paulfran.co>
2021-01-08 02:27:03 -0800

    add notes and get a single note

--- app/src/main/java/co/paulfran/ktor_note_app_client/ui/editnote/AddEditViewModel.kt
+++ app/src/main/java/co/paulfran/ktor_note_app_client/ui/editnote/AddEditViewModel.kt
@@ -1,6 +1,34 @@
 package co.paulfran.ktor_note_app_client.ui.editnote
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.paulfran.ktor_note_app_client.data.local.entities.Note
+import co.paulfran.ktor_note_app_client.other.Event
+import co.paulfran.ktor_note_app_client.other.Resource
+import co.paulfran.ktor_note_app_client.repositories.NoteRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class AddEditViewModel : ViewModel() {
+
+class AddEditViewModel @ViewModelInject constructor(
+        private val repository: NoteRepository
+) : ViewModel() {
+
+    private val _note = MutableLiveData<Event<Resource<Note>>>()
+    val note: LiveData<Event<Resource<Note>>> = _note
+
+    fun insertNote(note: Note) = GlobalScope.launch {
+        repository.insertNote(note)
+    }
+
+    fun getNoteById(noteId: String) = viewModelScope.launch {
+        _note.postValue(Event(Resource.loading(null)))
+        val note = repository.getNoteById(noteId)
+        note?.let {
+            _note.postValue(Event(Resource.success(it)))
+        } ?: _note.postValue(Event(Resource.error("Note not found", null)))
+    }
 }
