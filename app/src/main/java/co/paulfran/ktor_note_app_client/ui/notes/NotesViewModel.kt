3f8531e1628c236b2f1dfdd58e7299a5b29392ef
Paul Franco <mail@paulfran.co>
2021-01-08 04:35:22 -0800

    add and update notes

--- app/src/main/java/co/paulfran/ktor_note_app_client/ui/notes/NotesViewModel.kt
+++ app/src/main/java/co/paulfran/ktor_note_app_client/ui/notes/NotesViewModel.kt
@@ -1,6 +1,26 @@
 package co.paulfran.ktor_note_app_client.ui.notes
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import co.paulfran.ktor_note_app_client.data.local.entities.Note
+import co.paulfran.ktor_note_app_client.repositories.NoteRepository
+import co.paulfran.ktor_note_app_client.other.Event
+import co.paulfran.ktor_note_app_client.other.Resource
 
-class NotesViewModel : ViewModel() {
+
+class NotesViewModel @ViewModelInject constructor(
+        private val repository: NoteRepository
+): ViewModel() {
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _allNotes = _forceUpdate.switchMap {
+        repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+    }.switchMap {
+        MutableLiveData(Event(it))
+    }
+
+    val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+    fun syncAllNotes() = _forceUpdate.postValue(true)
 }
